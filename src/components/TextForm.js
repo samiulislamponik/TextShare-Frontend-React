c18b115c0aaff97657a2697c1b0f5e50ff7a12f6
Samiul Islam <samiul2238@gmail.com>
2023-05-12 01:38:48 +0600

    Some modifications are included like appearence of the url and text area turned into readonly mood for TextView

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,8 +1,45 @@
 import React from "react";
 
-const TextForm = ({ textSnippet, onSubmit, onTextChange, buttonText }) => {
+const TextForm = ({
+    textSnippet,
+    onSubmit,
+    onTextChange,
+    buttonText,
+    readOnly,
+    generatedUrl,
+    showUrl,
+    textCopied,
+    handleCopy,
+}) => {
     return (
         <form onSubmit={onSubmit}>
+            {/* text area for url */}
+            {showUrl && (
+                <div className="form-group mb-4">
+                    <label htmlFor="url">
+                        <h4>Url: </h4>
+                    </label>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="url"
+                            name="url"
+                            value={generatedUrl}
+                            readOnly={true}
+                        />
+                        <button
+                            className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={handleCopy}
+                        >
+                            {textCopied ? "Copied!" : "Copy"}
+                        </button>
+                    </div>
+                </div>
+            )}
+
+            {/* Text Area for Text Snippet */}
             <div className="form-group">
                 <label htmlFor="text">
                     <h3>Text Area: </h3>
@@ -14,9 +51,10 @@ const TextForm = ({ textSnippet, onSubmit, onTextChange, buttonText }) => {
                     rows="20"
                     onChange={onTextChange}
                     defaultValue={textSnippet?.content}
+                    readOnly={readOnly}
                 ></textarea>
             </div>
-            <button type="submit" className="btn btn-dark my-3">
+            <button type="submit" className="btn btn-dark my-4">
                 {buttonText}
             </button>
         </form>
--- src/components/TextView.js
+++ src/components/TextView.js
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import TextForm from "./TextForm";
 const apiUrl = process.env.REACT_APP_API_URL;
+const rootUrl = process.env.REACT_APP_ROOT_URL;
 
 const TextView = () => {
     const { url } = useParams();
     const navigate = useNavigate();
     const [textSnippet, setTextSnippet] = useState(null);
+    const [generatedUrl, setGeneratedUrl] = useState("");
+    const [copied, setCopied] = useState(false);
 
     useEffect(() => {
         const fetchText = async () => {
             const response = await fetch(`${apiUrl}/text/${url}`);
             const data = await response.json();
             setTextSnippet(data);
+            setGeneratedUrl(`${rootUrl}/t/${url}/`);
         };
         fetchText();
     }, [url]);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(generatedUrl);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
+    };
 
     const handleEditClick = (e) => {
         e.preventDefault();
@@ -28,6 +38,11 @@ const TextView = () => {
                 textSnippet={textSnippet}
                 onTextChange={() => {}}
                 onSubmit={handleEditClick}
+                generatedUrl={generatedUrl}
+                showUrl={true}
+                textCopied={copied}
+                handleCopy={handleCopy}
+                readOnly={true}
                 buttonText="Edit"
             />
         </div>
